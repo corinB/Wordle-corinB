cc88654ce23a46da1094b95b74cc7079e7bab58c
corinB <gkdisrha2020@gmail.com>
2026-07-28 15:50:30 +0900

    test: wordleService 정답 검증테스트 작성

---
+++ src/test/java/application/wordleServiceTest.java
@@ -0,0 +1,18 @@
+package application;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class wordleServiceTest {
+
+  @Test
+  @DisplayName("정답 검증 테스트 정답: apple")
+  void submitCorrectTest(){
+    WordleService wordleService = new WordleService();
+
+    assertThat(wordleService.submit("apple"), true);
+  }
+
+
+}
