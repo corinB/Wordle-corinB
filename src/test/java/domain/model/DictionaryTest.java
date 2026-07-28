257014f455dac0213c6c82c9c44a3f04cf98c221
corinB <gkdisrha2020@gmail.com>
2026-07-28 16:53:47 +0900

    test: word.txt에 있는 단어 중 시간을 기준으로 랜덤한 단어를 고르는 WordDictionary를 위한 테스트

---
+++ src/test/java/domain/model/DictionaryTest.java
@@ -0,0 +1,28 @@
+package domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DictionaryTest {
+
+  @Test
+  @DisplayName("지금 시간을 기준으로 랜덤 단어 선택")
+  void chooseCorrectWordTest(){
+
+    long seed1 = Instant.now().getEpochSecond();
+    long seed2 = Instant.now().plusSeconds(1).getEpochSecond();
+
+    WordDictionary dictionary = WordDictionary.getInstance();
+
+    Word word1 = dictionary.chooseCorrectWord(seed1);
+    Word word2 = dictionary.chooseCorrectWord(seed1);
+    Word word3 = dictionary.chooseCorrectWord(seed2);
+
+    assertThat(word1).isEqualTo(word2);
+    assertThat(word1).isNotEqualTo(word3);
+  }
+
+}
