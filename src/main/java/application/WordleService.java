1807f9e69a86fb64b5f01aa22b6a424fb04d21c0
corinB <gkdisrha2020@gmail.com>
2026-07-28 18:02:08 +0900

    refacto: 매개변수 중복을 제거하기 위한 도메인 모델 적용

--- src/main/java/application/WordleService.java
+++ src/main/java/application/WordleService.java
@@ -1,8 +1,30 @@
 package application;
 
-public class WordleService {
+import domain.model.Word;
+import domain.model.WordDictionary;
+import domain.repository.WordRepository;
 
-  boolean submit(String answor){
-    return answor == "apple";
+import java.time.Instant;
+
+public class WordleService<T extends WordRepository> {
+
+  private final WordDictionary<T> dictionary;
+  private Word correct;
+
+  public WordleService(WordDictionary<T> dictionary) {
+    this.dictionary = dictionary;
+  }
+
+  void gameStart(){
+    long seed = Instant.now().getEpochSecond();
+    correct = dictionary.chooseCorrectWord(seed);
+  }
+
+  boolean submit(Word answer){
+    return correct.equals(answer);
+  }
+
+  public Word getCorrect() {
+    return correct;
   }
 }
--- src/test/java/application/wordleServiceTest.java
+++ src/test/java/application/wordleServiceTest.java
@@ -1,18 +1,40 @@
 package application;
+import domain.model.Word;
+import domain.model.WordDictionary;
+import domain.repository.WordRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class wordleServiceTest {
 
   @Test
-  @DisplayName("정답 검증 테스트 정답: apple")
+  @DisplayName("정답 검증 테스트")
   void submitCorrectTest() {
-    WordleService wordleService = new WordleService();
+    WordleService wordleService = new WordleService(new WordDictionary(new MockWordRepository()));
 
-    assertThat(wordleService.submit("apple")).isTrue();
+    wordleService.gameStart();
+
+    Word correct = wordleService.getCorrect();
+
+    assertThat(wordleService.submit(correct)).isTrue();
   }
 
 
 }
+//가짜 래포지토리
+class MockWordRepository implements WordRepository {
+  @Override
+  public List<String> getAllWords() {
+    return List.of("apple","cocoa","mania", "radar", "green");
+  }
+
+  @Override
+  public int getTotalWordsCNT() {
+    return 6;
+  }
+}
+
