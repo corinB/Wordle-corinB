de33f7a81bdd02750dd58a40398254742593fb3f
corinB <gkdisrha2020@gmail.com>
2026-07-28 16:13:29 +0900

    test: 영단어 표현을 위한 word 객체 비교 테스트 작성

---
+++ src/test/java/domain/model/wordTest.java
@@ -0,0 +1,21 @@
+package domain.model;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class wordTest {
+
+  @Test
+  @DisplayName("비교")
+  void equalsTest(){
+    Word word1 = new word("apple");
+    Word word2 = new word("apple");
+    Word word3 = new word("applg");
+
+    assertThat(word1).isEqualTo(word2);
+    assertThat(word1).isNotEqualTo(word3);
+  }
+
+}
