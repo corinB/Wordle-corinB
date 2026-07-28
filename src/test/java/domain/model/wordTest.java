352b2c979d07b16c601301048fe62114fe2ce647
corinB <gkdisrha2020@gmail.com>
2026-07-28 16:25:18 +0900

    test: 영어 단어 생성자 매개변수 확인을 위한 테스트 작성

--- src/test/java/domain/model/wordTest.java
+++ src/test/java/domain/model/wordTest.java
@@ -19,4 +19,24 @@ public class wordTest {
     assertThat(word1).isEqualTo(word3);
     assertThat(word1).isNotEqualTo(word4);
   }
+
+  @Test
+  @DisplayName("영문으로만 생성 가능하며, 반드시 5글자이고 소문자로 저장된다")
+  void constructorParameterValidation() {
+    assertThatThrownBy(() -> new Word("appl"))
+      .isInstanceOf(IllegalArgumentException.class);
+
+    assertThatThrownBy(() -> new Word("appleee"))
+      .isInstanceOf(IllegalArgumentException.class);
+
+    assertThatThrownBy(() -> new Word("appl!"))
+      .isInstanceOf(IllegalArgumentException.class);
+
+    Word word1 = new Word("apple");
+    Word word2 = new Word("APPLE");
+
+    assertThat(word1.value()).isEqualTo("apple");
+    assertThat(word2.value()).isEqualTo("apple");
+    assertThat(word1).isEqualTo(word2);
+  }
 }
