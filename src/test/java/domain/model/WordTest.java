4441077a6f3ae25e1881b47d290aaaec80b47ed7
corinB <gkdisrha2020@gmail.com>
2026-07-28 16:35:11 +0900

    feat: 정답, 답변 등의 영어 단어 체크와 표현을 위한 Word 생성

--- src/test/java/domain/model/WordTest.java
+++ src/test/java/domain/model/WordTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class wordTest {
+public class WordTest {
 
   @Test
   @DisplayName("비교")
