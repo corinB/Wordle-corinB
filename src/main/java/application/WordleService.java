9a298ce290e3241c007b04a8737e37b09f94b382
corinB <gkdisrha2020@gmail.com>
2026-07-29 21:42:49 +0900

    feat: 게임 진행 관리 도메인 GameBord 추가 및 관련 기능 생성

--- src/main/java/application/WordleService.java
+++ src/main/java/application/WordleService.java
@@ -1,30 +1,42 @@
 package application;
 
+import domain.model.GameBoard;
 import domain.model.Word;
 import domain.model.WordDictionary;
 import domain.repository.WordRepository;
 
 import java.time.Instant;
+import java.util.List;
 
 public class WordleService {
 
   private final WordDictionary dictionary;
-  private Word correct;
+  private GameBoard gameBoard;
 
   public WordleService(WordRepository wordRepository) {
     this.dictionary = new WordDictionary(wordRepository);
   }
 
-  void gameStart(){
+  public void gameStart() {
     long seed = Instant.now().getEpochSecond();
-    correct = dictionary.chooseCorrectWord(seed);
+    Word correct = dictionary.chooseCorrectWord(seed);
+
+    gameBoard = new GameBoard(correct);
   }
 
-  boolean submit(Word answer){
-    return correct.equals(answer);
+  public void submit(Word answer) {
+    gameBoard.submit(answer);
   }
 
   public Word getCorrect() {
-    return correct;
+    return gameBoard.getCorrect();
+  }
+
+  public List<String> getRecords() {
+    return gameBoard.getRecords();
+  }
+
+  public int getSpentChance() {
+    return gameBoard.getSpentChance();
   }
 }
---
+++ src/main/java/domain/model/GameBoard.java
@@ -0,0 +1,47 @@
+package domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameBoard {
+
+  public static final int MAX_CHANCE = 6;
+
+  private final Word correct;
+  private final List<String> records;
+  private boolean correctFlag;
+
+  public GameBoard(Word correct) {
+    this.correct = correct;
+    this.records = new ArrayList<>();
+    this.correctFlag = false;
+  }
+
+  public void submit(Word answer) {
+    if (!canSubmit()) {
+      throw new IllegalStateException("끝난 게임입니다.");
+    }
+
+    records.add(correct.compare(answer));
+
+    if (correct.equals(answer)) {
+      correctFlag = true;
+    }
+  }
+
+  private boolean canSubmit() {
+    return !correctFlag && records.size() < MAX_CHANCE;
+  }
+
+  public Word getCorrect() {
+    return correct;
+  }
+
+  public int getSpentChance() {
+    return records.size();
+  }
+
+  public List<String> getRecords() {
+    return List.copyOf(records);
+  }
+}
