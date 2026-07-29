e772be57708b3d463c9f9ac9493e94da0a1926c0
corinB <gkdisrha2020@gmail.com>
2026-07-29 20:56:33 +0900

    test: 게임 상태변화 관리를 위한 테스트 추가

--- src/test/java/application/WordleServiceTest.java
+++ src/test/java/application/WordleServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 public class WordleServiceTest {
 
@@ -23,6 +25,68 @@ public class WordleServiceTest {
     assertThat(wordleService.submit(correct)).isTrue();
   }
 
+  @Test
+  @DisplayName("답안을 제출시 진행 기록 누적")
+  void getRecordTest() {
+    WordleService wordleService =
+      new WordleService(new MockWordRepository());
+
+    wordleService.gameStart();
+
+    Word correct = wordleService.getCorrect();
+    Word answer1 = new Word("aaaaa");
+    Word answer2 = new Word("bbbbb");
+
+    List<String> expected = new ArrayList<>();
+
+    expected.add(correct.compare(answer1));
+    wordleService.submit(answer1);
+
+    assertThat(wordleService.getRecords())
+      .isEqualTo(expected);
+
+    expected.add(correct.compare(answer2));
+    wordleService.submit(answer2);
+
+    assertThat(wordleService.getRecords())
+      .isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("정답시 답 제출 불가")
+  void cannotSubmitAfterCorrectAnswer() {
+    WordleService wordleService =
+      new WordleService(new MockWordRepository());
+
+    wordleService.gameStart();
+
+    Word correct = wordleService.getCorrect();
+
+    wordleService.submit(correct);
+
+    assertThatThrownBy(() ->
+      wordleService.submit(new Word("ccccc"))
+    ).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("기회를 모두 소진tl 답안을 제출 불가")
+  void cannotSubmitAfterLastChance() {
+    WordleService wordleService =
+      new WordleService(new MockWordRepository());
+
+    wordleService.gameStart();
+
+    Word wrongAnswer = new Word("aaaaa");
+
+    while (wordleService.canSubmit()) {
+      wordleService.submit(wrongAnswer);
+    }
+
+    assertThatThrownBy(() ->
+      wordleService.submit(new Word("ccccc"))
+    ).isInstanceOf(IllegalArgumentException.class);
+  }
 
 }
 //가짜 래포지토리
