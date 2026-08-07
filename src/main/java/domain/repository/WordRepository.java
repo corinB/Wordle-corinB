11b034b1a8e82a491eb7fafb0f57e6eefb72e622
corinB <gkdisrha2020@gmail.com>
2026-08-07 18:26:36 +0900

    refactor: word repository vo 계약 적용
    - 문자열 기반 WordRepository 메서드 제거
    - WordRepositoryImpl VO 기반으로 정리
    - WordEntity 생성 경로 VO로 제한

--- src/main/java/domain/repository/WordRepository.java
+++ src/main/java/domain/repository/WordRepository.java
@@ -7,16 +7,11 @@ import java.util.Optional;
 
 public interface WordRepository {
 
-  Optional<Word> findByWord(String value);
   Optional<Word> findByWord(Word word);
 
-  Word save(String value);
   Word save(Word word);
 
-
-  void delete(String value);
   void delete(Word word);
 
-  // words.txt 단어 전체 조회
   List<Word> findAll();
 }
