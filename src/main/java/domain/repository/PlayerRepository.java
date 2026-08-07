e06f854c4a60e7aed198296e8e21db7acc744c46
corinB <gkdisrha2020@gmail.com>
2026-08-07 18:17:57 +0900

    refactor: player repository 조회 계약 단순화
    - exists 기반 조회 계약 제거
    - Email VO 기반 조회 계약만 유지
    - PlayerRepository 테스트 수정

--- src/main/java/domain/repository/PlayerRepository.java
+++ src/main/java/domain/repository/PlayerRepository.java
@@ -1,7 +1,6 @@
 package domain.repository;
 import domain.model.Player;
 import domain.model.vo.Email;
-import domain.model.vo.Nickname;
 
 import java.util.Optional;
 
@@ -9,15 +8,5 @@ public interface PlayerRepository {
 
   Player save(Player player);
 
-  Optional<Player> findByEmail(String email);
-
   Optional<Player> findByEmail(Email email);
-
-  boolean existsByEmail(String email);
-
-  boolean existsByEmail(Email email);
-
-  boolean existsByNick(String nick);
-
-  boolean existsByNick(Nickname nick);
 }
