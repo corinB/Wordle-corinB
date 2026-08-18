e7e81a3526b903029e50fbeb07e2afff79f24a22
corinB <gkdisrha2020@gmail.com>
2026-08-18 16:15:24 +0900

    feat: 웹 컨트롤러 추가
    - 홈, 플레이어, 게임 컨트롤러 추가
    
    - 요청 DTO 기반 웹 입력 처리 추가
    
    - 세션 기반 로그인 상태 처리 추가

---
+++ src/main/java/presentation/dto/SubmitAnswerRequest.java
@@ -0,0 +1,14 @@
+package presentation.dto;
+
+public class SubmitAnswerRequest {
+
+  private String answer;
+
+  public String getAnswer() {
+    return answer;
+  }
+
+  public void setAnswer(String answer) {
+    this.answer = answer;
+  }
+}
